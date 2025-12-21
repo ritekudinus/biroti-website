68b3e88898212f30456625506c7c609757f7a855
aryaw05 <aryawahyuaw77@gmail.com>
2025-12-21 18:48:55 +0700

    feat(routes): add article and event routes with Inertia rendering
    Add new routes for article listing, article detail, and event listing pages using Inertia.js for frontend rendering. Includes temporary static route implementations.

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,28 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
+// STATIC ROUTES (SEMENTARA )
 Route::get('/', function () {
     return Inertia::render('welcome', [
         'canRegister' => Features::enabled(Features::registration()),
     ]);
 })->name('home');
+
+
+Route::get('/article', function () {
+    return Inertia::render('article/all-article');
+})->name('article.index');
+
+
+Route::get('/event', function () {
+    return Inertia::render('event/all-events');
+})->name('article.index');
+
+Route::get('/article/{slug}', function ($slug) {
+    return Inertia::render('article/detail-article');
+})->name('article.detail');
+
+
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
